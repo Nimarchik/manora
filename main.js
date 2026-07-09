ebde59fae169096bc4dc9f949f1f0cbfe3bf9356
Nimarchik <141066912+Nimarchik@users.noreply.github.com>
2026-07-09 21:49:36 +0300

    the end

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const btnNo = document.getElementById('btn_no')
 const btnYes = document.getElementById('btn_yes')
 const container = document.querySelector('.content')
 const message = document.getElementById('message')
+const vis = document.querySelector('.text_content')
 
 
 btnNo.addEventListener('mouseenter', () => {
@@ -102,4 +103,110 @@ inp.addEventListener("keyup", (event) => {
 });
 
 const moonAnimation = document.querySelector("#moonAnimation");
+let text = document.querySelector('.text_contentSub')
+let text1 = document.querySelector('.text_contentSub1')
+let text2 = document.querySelector('.text_contentSub2')
+let text3 = document.querySelector('.text_contentSub3')
 
+vis.addEventListener('click', () => {
+  text.classList.toggle('--active')
+  text1.classList.toggle('--active1')
+  text2.classList.toggle('--active2')
+  text3.classList.toggle('--active3')
+
+})
+
+
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext('2d');
+
+// for intro motion
+let mouseMoved = false;
+
+const pointer = {
+  x: .5 * window.innerWidth,
+  y: .5 * window.innerHeight,
+}
+const params = {
+  pointsNumber: 40,
+  widthFactor: .3,
+  mouseThreshold: .6,
+  spring: .4,
+  friction: .5
+};
+
+const trail = new Array(params.pointsNumber);
+for (let i = 0; i < params.pointsNumber; i++) {
+  trail[i] = {
+    x: pointer.x,
+    y: pointer.y,
+    dx: 0,
+    dy: 0,
+  }
+}
+
+window.addEventListener("click", e => {
+  updateMousePosition(e.pageX, e.pageY);
+});
+window.addEventListener("mousemove", e => {
+  mouseMoved = true;
+  updateMousePosition(e.pageX, e.pageY);
+});
+window.addEventListener("touchmove", e => {
+  mouseMoved = true;
+  updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+});
+
+function updateMousePosition(eX, eY) {
+  pointer.x = eX;
+  pointer.y = eY;
+}
+
+setupCanvas();
+update(0);
+window.addEventListener("resize", setupCanvas);
+
+
+function update(t) {
+
+  // for intro motion
+  if (!mouseMoved) {
+    pointer.x = (.5 + .3 * Math.cos(.002 * t) * (Math.sin(.005 * t))) * window.innerWidth;
+    pointer.y = (.5 + .2 * (Math.cos(.005 * t)) + .1 * Math.cos(.01 * t)) * window.innerHeight;
+  }
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  trail.forEach((p, pIdx) => {
+    const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
+    const spring = pIdx === 0 ? .4 * params.spring : params.spring;
+    p.dx += (prev.x - p.x) * spring;
+    p.dy += (prev.y - p.y) * spring;
+    p.dx *= params.friction;
+    p.dy *= params.friction;
+    p.x += p.dx;
+    p.y += p.dy;
+  });
+
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "#ffffff"; // Белый цвет
+  ctx.beginPath();
+  ctx.moveTo(trail[0].x, trail[0].y);
+
+  for (let i = 1; i < trail.length - 1; i++) {
+    const xc = .5 * (trail[i].x + trail[i + 1].x);
+    const yc = .5 * (trail[i].y + trail[i + 1].y);
+    ctx.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
+    ctx.lineWidth = params.widthFactor * (params.pointsNumber - i);
+    ctx.stroke();
+  }
+  ctx.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
+  ctx.stroke();
+
+  window.requestAnimationFrame(update);
+}
+
+function setupCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
